2ef02113291699716b263bc44eff0b6dde3287ac
nguyenthuydung1603 <nthuydung.1603@gmail.com>
2024-12-16 18:47:51 +0700

    [EPW-05] Refactor to reuse page

--- tests/admin_login.spec.js
+++ tests/admin_login.spec.js
@@ -2,11 +2,15 @@ const { test, expect } = require('@playwright/test');
 const {iStep} = require('../src/utils/steps-utils');
 const { AdminLoginPage } = require('../src/page/admin-login-page');
 const { DashboardPage } = require('../src/page/dashboard-page');
+let adminLoginPage;
+let dashboardPage;
+test.beforeAll('Before all', async ({page}) =>{
+  adminLoginPage= new AdminLoginPage(page);
+  dashboardPage = new DashboardPage(page);
+})
 
 // Bổ sung test case failed
 test('Verify admin login succesfully', async ({ page }) => {
-  let adminLoginPage= new AdminLoginPage(page);
-  let dashboardPage = new DashboardPage(page);
   await iStep(`I open Admin Page`,adminLoginPage.open());
   await iStep(`User should be on page Admin Login`, adminLoginPage.isOnPage());
   await iStep(`User input Email:`,adminLoginPage.inputTextByLabel('Email', 'admin@gmail.com'));
